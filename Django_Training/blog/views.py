eed12a44bd9f3e7df732e158f54119ff1be798a1
KINGNURA007 <arunrajamani190@gamil.com>
2026-07-19 23:08:37 +0530

    sunday practice

--- Django_Training/blog/views.py
+++ Django_Training/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    blog_title = "Latest Posts"
+    posts  = [
+        {'id':1, 'title': 'Post 1', 'content': 'Content of post 1'},
+        {'id':2, 'title': 'Post 2', 'content': 'Content of post 2'},
+        {'id':3, 'title': 'Post 3', 'content': 'Content of post 3'},
+        {'id':4, 'title': 'Post 4', 'content': 'Content of post 4'},        
+    ]
+    return render(request, 'index.html', {'blog_title': blog_title, "posts": posts})
+
+def detail(request, post_id):
+    return render(request, 'detail.html')
--- Django_Training/myapp/settings.py
+++ Django_Training/myapp/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_MYURL = [
+    "/blog/static/"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
